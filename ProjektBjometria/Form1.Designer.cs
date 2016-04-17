c8b93acb320aff8f945d57a17dc7cc8a193b66db
Piotr Gornik <gornikp94@gmail.com>
2016-04-17 13:16:47 +0200

    dodałem 2gą metodę scieniającą, która jest tak samo chujowa jak ta pierwsza XD

--- ProjektBjometria/Form1.Designer.cs
+++ ProjektBjometria/Form1.Designer.cs
@@ -30,10 +30,11 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonSavePicture = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.buttonSavePicture = new System.Windows.Forms.Button();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -52,57 +53,88 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.buttonSavePicture);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(110, 382);
+            this.panel1.Size = new System.Drawing.Size(110, 421);
             this.panel1.TabIndex = 1;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(110, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(523, 382);
-            this.panel2.TabIndex = 2;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox1.Image = global::ProjektBjometria.Properties.Resources.odcisk;
-            this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(523, 379);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // buttonSavePicture
             // 
-            this.buttonSavePicture.Location = new System.Drawing.Point(19, 84);
+            this.buttonSavePicture.Location = new System.Drawing.Point(19, 137);
             this.buttonSavePicture.Name = "buttonSavePicture";
             this.buttonSavePicture.Size = new System.Drawing.Size(75, 23);
             this.buttonSavePicture.TabIndex = 1;
             this.buttonSavePicture.Text = "Zapisz";
             this.buttonSavePicture.UseVisualStyleBackColor = true;
             this.buttonSavePicture.Click += new System.EventHandler(this.buttonSavePicture_Click);
-            //          
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(110, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(523, 421);
+            this.panel2.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.AllowDrop = true;
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::ProjektBjometria.Properties.Resources.odcisk;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 390);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
+            this.pictureBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragEnter);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(19, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(633, 382);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(633, 421);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -111,9 +143,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button buttonSavePicture;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- ProjektBjometria/Form1.cs
+++ ProjektBjometria/Form1.cs
@@ -8,12 +8,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace ProjektBjometria
 {
     public partial class Form1 : Form
     {
-        public Bitmap picture { get; set; }
+        public Bitmap Picture {
+            get
+            {
+                return picture;
+            }
+            set
+            {
+                picture = value;
+                pictureBox1.Image = picture;
+            }
+        }
+        private Bitmap picture;
         public Bitmap thinnedPicture { get; set; }
         public Form1()
         {
@@ -22,22 +35,55 @@ namespace ProjektBjometria
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int[,] table;
-            int[,] table2;
-            //picture = Properties.Resources.odcisk;
+            picture = Properties.Resources.odcisk;
             picture = (Bitmap)pictureBox1.Image;
             ThinningLibrary process = new ThinningLibrary();
-            table = process.BitmapToTable(picture);
-            table2 = process.doZhangSuenThinning(table, false);
-            thinnedPicture = process.TableToBitmap(table2);
+            //table = process.BitmapToTable(picture);
+            //table2 = process.doZhangSuenThinning(table, false);
+            //thinnedPicture = process.TableToBitmap(table2);
+            thinnedPicture = process.processImage((Bitmap)picture.Clone());
             picture = (Bitmap)thinnedPicture.Clone();
-            PixelFormat eeelo;
-            eeelo = thinnedPicture.PixelFormat;
             pictureBox1.Image = picture;
         }
         private void buttonSavePicture_Click(object sender, EventArgs e)
         {
             thinnedPicture.Save("C:\\Users\\BuBu\\Desktop\\02.png");
+        }
+
+        private void pictureBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void pictureBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                Picture = new Bitmap(((string[])e.Data.GetData(DataFormats.FileDrop, false))[0]);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("podano błędny plik " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[,] table;
+            int[,] table2;
+            picture = Properties.Resources.odcisk;
+            picture = (Bitmap)pictureBox1.Image;
+            ThinningLibrary process = new ThinningLibrary();
+            table = process.BitmapToTable(picture);
+            table2 = process.doZhangSuenThinning(table, false);
+            thinnedPicture = process.TableToBitmap(table2);          
+            picture = (Bitmap)thinnedPicture.Clone();          
+            pictureBox1.Image = picture;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
